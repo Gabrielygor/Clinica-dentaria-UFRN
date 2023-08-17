335ec3284272a7f54a28b33fa004cf4b4c29f05d
Gabrielygor <gabrielcanuto042@gmail.com>
2023-08-17 19:31:17 -0300

    Tela Inicial (Teste GitHub)

---
+++ main.c
@@ -0,0 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte (UFRN)          ///
+///                 Centro de Ensino Superior do Seridó (CERES)             ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão para clinicas dentarias      ///
+///                      Developed by @Gabrielygor                          ///
+///////////////////////////////////////////////////////////////////////////////
+///  Trabalho referente ao segundo semestre do ano de 2023 do curso de BSI  ///
+///////////////////////////////////////////////////////////////////////////////
