9db405b996d81ffbdf891fc49db7647f5460137d
Gabrielygor <gabrielcanuto042@gmail.com>
2023-08-19 14:08:37 -0300

    adicionado tela sobre informações do trabalho

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 ///                 Centro de Ensino Superior do Seridó (CERES)             ///
 ///               Departamento de Computação e Tecnologia                   ///
 ///                  Disciplina DCT1106 -- Programação                      ///
-///                  Projeto Sistema de Gestão para clinicas dentarias      ///
+///             Projeto Sistema de Gestão para clinicas dentarias           ///
 ///                      Developed by @Gabrielygor                          ///
 ///////////////////////////////////////////////////////////////////////////////
 ///  Trabalho referente ao segundo semestre do ano de 2023 do curso de BSI  ///
@@ -14,20 +14,22 @@
 #include <stdlib.h>    // Biblioteca
 
 
-void tela_menu_p(void);
+void tela_menu_p(void);     //Similar as "DEFS" em Python
+void info_trabalho(void);
 
 
 int main(void){
     tela_menu_p();
+    info_trabalho();
     return 0;
 }
 
-void tela_menu_p(void) {
+void tela_menu_p(void) {    //Tela principal do programa
     char op;
     system("clear||cls");
     printf("\n");
     printf("===============================================\n");
-    printf("   SISTEMA DE GESTAO PARA CLINICAS DENTARIAS\n");
+    printf("   SISTEMA DE GESTAO PARA CLINICAS DENTARIAS   \n");
     printf("===============================================\n");
     printf("\n");
     printf("[1].Gerenciamento/Cadastro de clientes\n");
@@ -35,15 +37,31 @@ void tela_menu_p(void) {
     printf("[3].\n");
     printf("[4].\n");
     printf("[5].Agendamento de horarios\n");
-    printf("[6].Gestão de estoque\n");
+    printf("[6].Gestao de estoque\n");
     printf("[0].Sair\n");
-   
-    printf("///            Escolha a opção desejada: ");
+    printf("\n");   
+    printf("Escolha a opcao desejada: ");
     scanf("%c", &op);
     getchar();
     printf("\n");
-    printf("\n");
-    printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
+}
+
+
+
+
+void info_trabalho(void){
+    system("clear||cls");
+    printf("\n");
+    printf("=============================\n");
+    printf("   Inforamcoes do Trabalho \n");
+    printf("=============================\n");
+    printf("    Este programa aborda o conteudo do segundo semestre de 2023 da disciplina de programacao do curso de\n");
+    printf("Bacharelado em Sistemas de Informacao (BSI) na Universidade do Rio Grande do Norte(UFRN)\n");
+    printf("\n");
+    printf("Este programa representa uma solucao de software desenvolvida em linguagem C para a gestao de clinicas\n");
+    printf("odontologicas. O sistema é projetado para auxiliar a administracao eficiente das atividades de uma\n");
+    printf("clinica dentaria\n");
+
 }
