904316899391f83cd6acd16e7ef3a6eacfa09837
Gabrielygor <gabrielcanuto042@gmail.com>
2023-08-20 08:54:49 -0300

    Menu de Clientes adicionado

--- main.c
+++ main.c
@@ -14,22 +14,23 @@
 #include <stdlib.h>    // Biblioteca
 
 
-void tela_menu_p(void);     //Similar as "DEFS" em Python
+void tela_menu_p(void);     
 void info_trabalho(void);
 void equipe(void);
+void cliente(void);                                 
 
 
 int main(void){
     tela_menu_p();
     info_trabalho();
     equipe();
+    cliente();
     return 0;
 }
 
 void tela_menu_p(void) {    //Tela principal do programa
     char op;
     system("clear||cls");
-    printf("\n");
     printf("===============================================\n");
     printf("   SISTEMA DE GESTAO PARA CLINICAS DENTARIAS   \n");
     printf("===============================================\n");
@@ -50,16 +51,15 @@ void tela_menu_p(void) {    //Tela principal do programa
 
 void info_trabalho(void){
     system("clear||cls");
-    printf("\n");
     printf("=============================\n");
     printf("   Inforamcoes do Trabalho   \n");
     printf("=============================\n");
-    printf("    Este programa aborda o conteudo do segundo semestre de 2023 da disciplina de programacao do curso de\n");
-    printf("Bacharelado em Sistemas de Informacao (BSI) na Universidade do Rio Grande do Norte(UFRN)\n");
     printf("\n");
-    printf("Este programa representa uma solucao de software desenvolvida em linguagem C para a gestao de clinicas\n");
+    printf("    Este programa aborda o conteudo do segundo semestre de 2023 da disciplina de programacao do curso de\n");
+    printf("Bacharelado em Sistemas de Informacao (BSI) na Universidade do Rio Grande do Norte(UFRN).\n");
+    printf("E tambem representa uma solucao de software desenvolvida em linguagem C para a gestao de clinicas\n");
     printf("odontologicas. O sistema e projetado para auxiliar a administracao eficiente das atividades de uma\n");
-    printf("clinica dentaria\n");
+    printf("clinica dentaria.\n");
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
@@ -68,8 +68,9 @@ void info_trabalho(void){
 void equipe(void){
     system("clear||cls");
     printf("============\n");
-    printf("   Equipe    \n");
+    printf("   Equipe   \n");
     printf("============\n");
+    printf("\n");
     printf("|Projeto desenvolvido por:\n");
     printf("\n");
     printf("|Gabriel Ygor Canuto\n");
@@ -79,5 +80,25 @@ void equipe(void){
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
+}
 
+void cliente(void){
+    char op;
+    system("clear||cls");
+    printf("===============================\n");
+    printf("   Gerenciamento de Clientes   \n");
+    printf("===============================\n");
+    printf("\n");
+    printf("|[1]. Cadastrar Cliente\n");
+    printf("|[2]. Pesquisar Cliente\n");
+    printf("|[3]. Atualizar Cliente\n");
+    printf("|[4]. Excluir Cliente\n");
+    printf("|[0]. Voltar ao menu anterior\n");
+    printf("\n");
+    printf("|Escolha a opcao desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 }
