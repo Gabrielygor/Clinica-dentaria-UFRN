7cf0e2f9bd7593e7a523d5cf448bc83253db4e4f
Gabrielygor <gabrielcanuto042@gmail.com>
2023-09-16 16:44:17 -0300

    Adicionado modularização menu funcionario

---
+++ funcionario.c
@@ -0,0 +1,101 @@
+#include <stdio.h>      //Biblioteca 
+#include <stdlib.h> 
+#include "funcionario.h"
+
+void funcionario(void){
+    char op;
+    do {
+        system("clear||cls");
+        printf("===================================\n");
+        printf("\n");
+        printf("   Gerenciamento de Funcion�rios   \n");
+        printf("\n");
+        printf("===================================\n");
+        printf("\n");
+        printf("|[1]. Cadastrar Funcionário\n");
+        printf("|[2]. Pesquisar Funcionário\n");
+        printf("|[3]. Atualizar Funcionário\n");
+        printf("|[4]. Excluir Funcionário\n");
+        printf("|[5]. Listar Funcionários\n");
+        printf("|[0]. Voltar ao menu Principal\n");
+        printf("\n");
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        printf("\n");
+        printf("|Escolha a opção desejada: ");
+        scanf(" %c", &op);
+        getchar();
+    
+        switch (op){
+          case '1':
+              cadastrofun();
+              break;
+          case '2':
+              pesquisafun();
+              break;
+          case '3':
+              atualizafun();
+              break;
+          case '4':
+              excluirfun();
+              break;
+          case '5':
+              listafun();
+              break;
+          case '0':
+              tela_menu_p();
+              break;
+          default:
+              printf("Opção inválida! Tente novamente.\n");
+      }     
+    } while (op != '0');
+}
+
+void cadastrofun(void){
+    system("clear||cls");
+    printf("Cadastro Em construção...");
+    getchar();
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+
+}
+
+void pesquisafun(void){
+    system("clear||cls");
+    printf("Pesquisa Em construção...");
+    getchar();
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+
+}
+
+void atualizafun(void){
+    system("clear||cls");
+    printf("Atualizar Em construção...");
+    getchar();
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+
+}
+
+void listafun(void){
+    system("clear||cls");
+    printf("Listar Em construção...");
+    getchar();
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+
+}
+
+void excluirfun(void){
+    system("clear||cls");
+    printf("Excluir Em construção...");
+    getchar();
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+
+}
--- funcionario.h
+++ funcionario.h
@@ -1,6 +1,11 @@
+#ifndef FUNCIONARIO_H
+#define FUNCIONARIO_H
+
 void funcionario(void);           //Assinatura de Funcionarios 
 void cadastrofun(void);
 void pesquisafun(void);
 void atualizafun(void);
 void listafun(void);
 void excluirfun(void);
+
+#endif
