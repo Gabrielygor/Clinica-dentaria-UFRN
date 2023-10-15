9319991a5d98a764ae66c1b6c1b8a29fa0685060
Gabrielygor <gabrielcanuto042@gmail.com>
2023-10-15 19:49:57 -0300

    Adicionado a Struct de agendamento

--- agendamento.h
+++ agendamento.h
@@ -1,3 +1,10 @@
+struct agendamento{
+    char cpf[12];
+    char data[11];
+    char hora[5];
+    char funcionario[10];
+};
+
 void agendamento(void);        // Assinatura de Agendamentos
 void agendarconsulta(void); 
 void agendarretorno(void);
