9b14138b06f6a54d5c63cac4774b30cda8e4e31f
Gabrielygor <gabrielcanuto042@gmail.com>
2023-11-07 06:13:04 -0300

    Adicionado CRUD basico no modulo de estoque

--- estoque.c
+++ estoque.c
@@ -3,8 +3,8 @@
 #include "estoque.h"
 #include "telas.h"
 #include "ulti.h"
+#include <string.h>
 
-//O ESTOQUE SERA FEITO POR ULTIMO, VISTO QUE, EM QUESTOES AVALIATIVAS SAO NECESSARIOS APENAS OS MODULOS DE CLIENTES, FUNCIONARIOS E AGENDAMENTOS
 
 void estoque(void){
     char op;
@@ -19,12 +19,14 @@ void estoque(void){
         printf("|[1]. Adicionar Estoque\n");
         printf("|[2]. Excluir Estoque\n");
         printf("|[3]. Listar Estoque\n");
+        printf("|[4]. Editar Estoque\n");
+        printf("|[5]. Pesquisar Estoque\n");
         printf("|[0]. Voltar ao menu Principal\n");
         printf("\n");
         printf("=-=-=-=-=-=-=\n");
         printf("\n");
-        printf("|Escolha a opção desejada: ");
-        scanf(" %c", &op);
+        printf("|Escolha a opcao desejada: ");
+        scanf(" %s", &op);
         getchar();
 
         switch (op){
@@ -37,6 +39,12 @@ void estoque(void){
           case '3':
             listarestoque();
             break;
+          case '4':
+            alterarestoque();
+            break;
+          case '5':
+            pesquisarestoque();
+            break;
           case '0':
             break;
           default:
@@ -48,40 +56,224 @@ void estoque(void){
       }  while (op != '0');
 }
 
-void adicionarestoque(void){
-    system("clear||cls");
-    printf("Digite o nome do objeto:\n");
-    printf("Digite a quantidade do objeto:\n");
-    printf("Digite um ID para o objeto:\n");
-    printf("\n");
-    printf("EM CONSTRUÇÃO...");
-    getchar();
-    printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    getchar();
-
+void saveObjetoEstoque(ObjetoEstoque* objeto) {
+    FILE* file = fopen("estoque.dat", "ab");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo para escrita.\n");
+        return;
+    }
+    fwrite(objeto, sizeof(ObjetoEstoque), 1, file);
+    fclose(file);
 }
 
-void excluirestoque(void){
+// Função para listar os objetos no estoque
+void listarestoque(void) {
+    ObjetoEstoque objeto;
     system("clear||cls");
-    printf("Digite o nome do objeto que deseja excluir:\n");
+    printf("=========================\n");
+    printf("   Lista de Objetos   \n");
+    printf("=========================\n");
     printf("\n");
-    printf("EM CONSTRUÇÃO...");
-    getchar();
+
+    FILE* file = fopen("estoque.dat", "rb");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo para leitura.\n");
+        return;
+    }
+
+    while (fread(&objeto, sizeof(ObjetoEstoque), 1, file) == 1) {
+        if (objeto.ativo == 1) {
+            printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+            printf("Nome: %s\n", objeto.nome);
+            printf("Quantidade: %d\n", objeto.quantidade);
+            printf("ID: %d\n", objeto.id);
+            printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        }
+    }
+
+    fclose(file);
+
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
-
 }
 
-void listarestoque(void){
+// Função para adicionar um objeto ao estoque
+void adicionarestoque(void) {
+    ObjetoEstoque objeto;
+
     system("clear||cls");
-    printf("Lista de obejtos cadastrados no sistema:\n");
+    printf("===========================\n");
+    printf("   Adicionar ao Estoque   \n");
+    printf("===========================\n");
+
+    printf("Digite o nome do objeto: ");
+    scanf("%s", objeto.nome);
+
+    printf("Digite a quantidade do objeto: ");
+    scanf("%d", &objeto.quantidade);
+
+    printf("Digite um ID para o objeto: ");
+    scanf("%d", &objeto.id);
+
+    objeto.ativo = 1;
+
+    saveObjetoEstoque(&objeto);
+
     printf("\n");
-    printf("EM CONSTRUÇÃO...");
+    printf("Objeto adicionado ao estoque com sucesso.\n");
     getchar();
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
+}
 
+// Função para excluir um objeto do estoque
+void excluirestoque(void) {
+    char nome[100];
+    ObjetoEstoque objeto;
+    int objetoEncontradoFlag = 0;
+
+    system("clear||cls");
+    printf("=========================\n");
+    printf("   Excluir do Estoque   \n");
+    printf("=========================\n");
+
+    printf("Digite o nome do objeto que deseja excluir: ");
+    scanf("%s", nome);
+
+    FILE* file = fopen("estoque.dat", "rb+");
+
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo para leitura e escrita.\n");
+        return;
+    }
+
+    while (fread(&objeto, sizeof(ObjetoEstoque), 1, file) == 1) {
+        if (strcmp(objeto.nome, nome) == 0) {
+            objeto.ativo = 0;
+            fseek(file, -sizeof(ObjetoEstoque), SEEK_CUR);
+            fwrite(&objeto, sizeof(ObjetoEstoque), 1, file);
+            objetoEncontradoFlag = 1;
+            break;
+        }
+    }
+
+    if (!objetoEncontradoFlag) {
+        printf("\n");
+        printf("Objeto com nome %s não encontrado.\n", nome);
+        getchar();
+        printf("\n");
+        printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    } else {
+        printf("\n");
+        printf("Objeto excluído do estoque com sucesso.\n");
+        getchar();
+        printf("\n");
+        printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    }
+
+    fclose(file);
+    getchar();
+}
+
+void alterarestoque(void) {
+    char nome[100];
+    ObjetoEstoque objeto;
+    int objetoEncontradoFlag = 0;
+
+    system("clear||cls");
+    printf("===========================\n");
+    printf("   Alterar no Estoque   \n");
+    printf("===========================\n");
+
+    printf("Digite o nome do objeto que deseja alterar: ");
+    scanf("%s", nome);
+
+    FILE* file = fopen("estoque.dat", "rb+");
+
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo para leitura e escrita.\n");
+        return;
+    }
+
+    while (fread(&objeto, sizeof(ObjetoEstoque), 1, file) == 1) {
+        if (strcmp(objeto.nome, nome) == 0) {
+            objeto.ativo = 0;
+            fseek(file, -sizeof(ObjetoEstoque), SEEK_CUR);
+            printf("Digite o novo nome do objeto: ");
+            scanf("%s", objeto.nome);
+            printf("Digite a nova quantidade do objeto: ");
+            scanf("%d", &objeto.quantidade);
+            printf("Digite o novo ID do objeto: ");
+            scanf("%d", &objeto.id);
+            objeto.ativo = 1;
+            fseek(file, -sizeof(ObjetoEstoque), SEEK_CUR);
+            fwrite(&objeto, sizeof(ObjetoEstoque), 1, file);
+            objetoEncontradoFlag = 1;
+            break;
+        }
+    }
+
+    if (!objetoEncontradoFlag) {
+        printf("\n");
+        printf("Objeto com nome %s nao encontrado.\n", nome);
+        getchar();
+        printf("\n");
+        printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    } else {
+        printf("\n");
+        printf("Objeto alterado no estoque com sucesso.\n");
+        getchar();
+        printf("\n");
+        printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    }
+
+    fclose(file);
+    getchar();
+}
+
+// Função para pesquisar objetos no estoque
+void pesquisarestoque(void) {
+    char nome[100];
+    ObjetoEstoque objeto;
+    int objetoEncontradoFlag = 0;
+
+    system("clear||cls");
+    printf("===========================\n");
+    printf("   Pesquisar no Estoque   \n");
+    printf("===========================\n");
+
+    printf("Digite o nome do objeto que deseja pesquisar: ");
+    scanf("%s", nome);
+
+    FILE* file = fopen("estoque.dat", "rb");
+
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo para leitura.\n");
+        return;
+    }
+
+    while (fread(&objeto, sizeof(ObjetoEstoque), 1, file) == 1) {
+        if (strcmp(objeto.nome, nome) == 0) {
+            printf("\n");
+            printf("Nome: %s\n", objeto.nome);
+            printf("Quantidade: %d\n", objeto.quantidade);
+            printf("ID: %d\n", objeto.id);
+            printf("\n");
+            objetoEncontradoFlag = 1;
+            break;
+        }
+    }
+
+    if (!objetoEncontradoFlag) {
+        printf("\n");
+        printf("Objeto com nome %s nao encontrado no estoque.\n", nome);
+    }
+
+    fclose(file);
+    getchar();
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 }
--- estoque.h
+++ estoque.h
@@ -1,4 +1,14 @@
+typedef struct {
+    char nome[100];      // Nome do objeto
+    int quantidade;      // Quantidade do objeto
+    int id;              // ID do objeto
+    int ativo;           // Indica se o objeto está ativo (1) ou inativo (0)
+} ObjetoEstoque;
+
 void estoque(void);            //Assinatura de Estoque 
 void adicionarestoque(void);
 void excluirestoque(void);
 void listarestoque(void);
+void saveObjetoEstoque(ObjetoEstoque* objeto);
+void alterarestoque(void);
+void pesquisarestoque(void);
